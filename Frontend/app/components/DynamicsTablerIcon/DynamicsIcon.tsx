4192387a6c86c2ab31912444bcd4c868a9a363d8
Sedenter <bagasuwaidha007@gmail.com>
2025-07-16 21:08:36 +0700

    grid services fixedd

--- Frontend/app/components/DynamicsTablerIcon/DynamicsIcon.tsx
+++ Frontend/app/components/DynamicsTablerIcon/DynamicsIcon.tsx
@@ -3,5 +3,5 @@ import * as TablerIcons from '@tabler/icons-react';
 export default function DynamicsIcon({ iconName }: { iconName: string }) {
   const IconComponent = (TablerIcons as any)[iconName];
   if (!IconComponent) return null;
-  return <IconComponent size={32} />;
+  return <IconComponent size={28} />;
 }
