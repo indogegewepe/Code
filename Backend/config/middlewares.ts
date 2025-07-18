2f16ee2c484c0ebb34d61e5affaf51600f211940
Sedenter <bagasuwaidha007@gmail.com>
2025-07-19 04:28:55 +0700

    beken

--- Backend/config/middlewares.ts
+++ Backend/config/middlewares.ts
@@ -3,27 +3,11 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: [process.env.NEXT_PUBLIC_FRONTEND_URL],
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    },
-  },
-  'strapi::logger',
-  'strapi::security',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
-
-module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [process.env.NEXT_PUBLIC_FRONTEND_URL],
+      enabled: true,
+      origin: process.env.NEXT_PUBLIC_FRONTEND_URL ? process.env.NEXT_PUBLIC_FRONTEND_URL.split(',') : [],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       credentials: true,
+      headers: '*',
     },
   },
   'strapi::security',
